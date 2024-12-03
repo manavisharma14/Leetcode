d1c30e1eb6344b4da06e54ea57d9815a3151007a
Manavi Sharma <75850906+manavisharma14@users.noreply.github.com>
2024-12-03 17:59:30 -0600

    Time: 1 ms (86.36%), Space: 14.9 MB (11.64%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,12 +1,16 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> num_maps;
         for(int i=0; i<nums.size(); i++){
-            for(int j=0; j<nums.size(); j++){
-                if(i!=j && nums[i]+nums[j] == target){
-                    return {i,j};
+            int complement = target - nums[i];
+        
+                if(num_maps.find(complement) != num_maps.end()){
+                
+                return {num_maps[complement], i};
                 }
-            }
+                num_maps[nums[i]] = i;
+            
         }
         return {};
     }
