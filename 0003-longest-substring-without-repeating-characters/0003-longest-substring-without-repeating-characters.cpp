e19158c77ef99c41a70a02186d48e47a64484f35
Manavi Sharma <75850906+manavisharma14@users.noreply.github.com>
2024-12-05 16:18:32 -0600

    Time: 15 ms (46.76%), Space: 14.3 MB (37.46%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -8,18 +8,17 @@ public:
         int maxLength = 0;
 
         for(int right=0; right<n; right++){
-            if (charSet.count(s[right]) == 0){
+            if(charSet.count(s[right]) == 0){
                 charSet.insert(s[right]);
                 maxLength = max(maxLength, right-left+1);
             }
-            else {
-                while(charSet.count(s[right])){
-                    charSet.erase(s[left]);
-                    left++;
-                }
-                charSet.insert(s[right]);
+            else while(charSet.count(s[right])){
+                charSet.erase(s[left]);
+                left++;
             }
-        } 
+            charSet.insert(s[right]);
+        }
+    
         
         return maxLength;
     }
