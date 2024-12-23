dc0d39b108ea6f2ee1ec7b7e46358b0cb1bf6f8d
Manavi Sharma <75850906+manavisharma14@users.noreply.github.com>
2024-12-23 16:26:14 -0600

    Time: 0 ms (100%), Space: 10 MB (18.09%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string ans;
+
+        for(char c: s){
+            if(c == ' ') continue;
+            if(isalnum(c)){
+                ans+=tolower(c);
+            }
+        }
+        int n = ans.size();
+        for(int i=0; i<n/2; i++){
+            if(ans[i] != ans[n-1-i]){
+                return false;
+            }
+        }
+        return true;
+    }
+};
