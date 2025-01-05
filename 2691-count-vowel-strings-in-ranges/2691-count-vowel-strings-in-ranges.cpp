abb455be6c5d48986285b95772f2769332a63ee9
Manavi Sharma <75850906+manavisharma14@users.noreply.github.com>
2025-01-05 01:12:18 -0600

    Time: 4 ms (70.23%), Space: 70 MB (51.81%) - LeetHub

--- 2691-count-vowel-strings-in-ranges/2691-count-vowel-strings-in-ranges.cpp
+++ 2691-count-vowel-strings-in-ranges/2691-count-vowel-strings-in-ranges.cpp
@@ -5,30 +5,31 @@ public:
         c = tolower(c);
         return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
     }
+
     vector<int> vowelStrings(vector<string>& words, vector<vector<int>>& queries) {
         
         int n = words.size();
-        vector<int> prefixSum(n + 1, 0);
+        vector<int>prefixsum(n+1, 0);
 
         for(int i=0; i<words.size(); i++){
             const string &word = words[i];
+
             if (isVowel(word[0]) && isVowel(word[word.length() - 1])) {
-                prefixSum[i+1] = prefixSum[i] + 1;
-            }
-            else{
-                prefixSum[i+1] = prefixSum[i];
+                prefixsum[i+1] = prefixsum[i] + 1;
+            } 
+            else {
+                prefixsum[i+1] = prefixsum[i];
             }
         }
 
 
         vector<int>result;
 
-        for(const auto&query: queries){
+        for(const auto& query: queries){
             int li = query[0];
             int ri = query[1];
 
-            
-            result.push_back(prefixSum[ri+1] - prefixSum[li]);
+            result.push_back(prefixsum[ri+1] - prefixsum[li]);
         }
         return result;
     }
