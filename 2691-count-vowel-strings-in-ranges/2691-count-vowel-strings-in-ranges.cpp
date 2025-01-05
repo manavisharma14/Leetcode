4ce7c9f22f3e6dc35b60d299eb6c5089fff88f31
Manavi Sharma <75850906+manavisharma14@users.noreply.github.com>
2025-01-05 00:58:00 -0600

    Time: 4 ms (70.23%), Space: 70 MB (62.4%) - LeetHub

---
+++ 2691-count-vowel-strings-in-ranges/2691-count-vowel-strings-in-ranges.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+
+    bool isVowel(char c){
+        c = tolower(c);
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+    }
+    vector<int> vowelStrings(vector<string>& words, vector<vector<int>>& queries) {
+        
+        int n = words.size();
+        vector<int> prefixSum(n + 1, 0);
+
+        for(int i=0; i<words.size(); i++){
+            const string &word = words[i];
+            if (isVowel(word[0]) && isVowel(word[word.length() - 1])) {
+                prefixSum[i+1] = prefixSum[i] + 1;
+            }
+            else{
+                prefixSum[i+1] = prefixSum[i];
+            }
+        }
+
+
+        vector<int>result;
+
+        for(const auto&query: queries){
+            int li = query[0];
+            int ri = query[1];
+
+            
+            result.push_back(prefixSum[ri+1] - prefixSum[li]);
+        }
+        return result;
+    }
+};
