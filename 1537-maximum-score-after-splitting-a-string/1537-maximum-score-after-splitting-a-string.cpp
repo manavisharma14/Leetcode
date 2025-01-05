40691c1c105d7abe1ee84159be3fd2707f9998d3
Manavi Sharma <75850906+manavisharma14@users.noreply.github.com>
2025-01-04 23:42:43 -0600

    Time: 0 ms (100%), Space: 9 MB (19.91%) - LeetHub

--- 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.cpp
+++ 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.cpp
@@ -1,28 +1,23 @@
 class Solution {
 public:
     int maxScore(string s) {
-        int n = s.length();
+        int n=s.length();
 
-        int sum0 = 0, sum1 = 0, ans = 0;
+        int sum0 = 0, sum1 = 0;
+        int ans=0;
 
-        // Count the total number of '1's in the string
-        for (int i = 0; i < n; i++) {
-            if (s[i] == '1') {
+        for(int i=0; i<n; i++){
+            if(s[i] == '1'){
                 sum1++;
             }
         }
 
-        // Iterate through the string to calculate scores
-        // Stop at n - 1 to ensure valid split
-        for (int i = 0; i < n - 1; i++) {
-            if (s[i] == '0') {
-                sum0++;
-            } else if (s[i] == '1') {
-                sum1--;
-            }
-            ans = max(ans, sum0 + sum1);
-        }
+        for(int i=0; i<n-1; i++){
+            if(s[i] == '0') sum0++;
+            else if(s[i] == '1') sum1--;
 
+            ans = max(ans, sum0+sum1);
+        }
         return ans;
     }
 };
