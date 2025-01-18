283bf50dce03297573a57f14dfb6a23666895a3c
Manavi Sharma <75850906+manavisharma14@users.noreply.github.com>
2025-01-18 15:28:43 -0600

    Time: 0 ms (100%), Space: 64.3 MB (72.48%) - LeetHub

--- 2533-bitwise-xor-of-all-pairings/2533-bitwise-xor-of-all-pairings.cpp
+++ 2533-bitwise-xor-of-all-pairings/2533-bitwise-xor-of-all-pairings.cpp
@@ -4,21 +4,23 @@ public:
         int xor1 = 0, xor2 = 0;
         int n1 = nums1.size(), n2 = nums2.size();
         
-        // Compute XOR of nums1
-        for (int num : nums1) {
+        for(int num: nums1){
             xor1 ^= num;
         }
 
-        // Compute XOR of nums2
-        for (int num : nums2) {
+        for(int num: nums2){
             xor2 ^= num;
         }
 
-        // Calculate result based on the lengths of nums1 and nums2
         int result = 0;
-        if (n1 % 2 == 1) result ^= xor2; // Include nums2 if nums1 length is odd
-        if (n2 % 2 == 1) result ^= xor1; // Include nums1 if nums2 length is odd
 
+        if(n1%2 == 1){
+            result ^= xor2;
+        }
+
+        if(n2%2 == 1){
+            result ^= xor1;
+        }
         return result;
     }
 };
