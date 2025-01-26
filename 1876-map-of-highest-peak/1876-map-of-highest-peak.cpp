2624586808c95ede11fb70dc75409653c897fdb9
Manavi Sharma <75850906+manavisharma14@users.noreply.github.com>
2025-01-26 00:26:02 -0600

    Time: 72 ms (55.89%), Space: 112.9 MB (39.58%) - LeetHub

--- 1876-map-of-highest-peak/1876-map-of-highest-peak.cpp
+++ 1876-map-of-highest-peak/1876-map-of-highest-peak.cpp
@@ -3,15 +3,13 @@ public:
 
     
     vector<vector<int>> highestPeak(vector<vector<int>>& isWater) {
-        // implementing bfs
-
         int rows = isWater.size();
         int cols = isWater[0].size();
 
-        vector<vector<int>> grid(rows, vector<int>(cols, -1));
+        vector<vector<int>>grid(rows, vector<int>(cols, -1));
 
         for(int i=0; i<rows; i++){
-            for(int j=0; j<cols; j++){
+            for (int j=0; j<cols; j++){
                 if(isWater[i][j] == 1){
                     grid[i][j] = 0;
                 }
@@ -21,29 +19,28 @@ public:
         queue<pair<int,int>>q;
 
         for(int i=0; i<rows; i++){
-            for(int j=0; j<cols; j++){
+            for (int j=0; j<cols; j++){
                 if(grid[i][j] == 0){
-                    q.push({i,j});
-                }
+                q.push({i,j});
             }
         }
+        }
 
-        int directions[4][2] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        int directions[4][2] = {{1,0}, {-1,0}, {0,1}, {0,-1}};
 
         while(!q.empty()){
             auto [x, y] = q.front(); q.pop();
 
             for(auto [dx, dy] : directions){
-                int nx = x + dx, ny = y + dy;
-                if(nx >=0 && nx < rows && ny >=0 && ny < cols && grid[nx][ny] == -1){
+                int nx = x + dx;
+                int ny = y + dy;
+
+                if(nx >= 0 && nx < rows && ny >=0 && ny < cols && grid[nx][ny] == -1){
                     grid[nx][ny] = grid[x][y] + 1;
-                    q.push({nx,ny});
+                    q.push({nx, ny});
                 }
             }
-
-
         }
-        return grid;
-
+return grid;
     }
 };
