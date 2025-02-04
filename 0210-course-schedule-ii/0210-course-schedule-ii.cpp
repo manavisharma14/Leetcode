e420ca743e0f4df46b85d6176ac200294b074ca7
Manavi Sharma <75850906+manavisharma14@users.noreply.github.com>
2025-02-04 17:51:41 -0600

    Time: 8 ms (26.2%), Space: 17.9 MB (92.15%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,16 +1,11 @@
-#include <vector>
-#include <queue>
-#include <algorithm>
-using namespace std;
-
 class Solution {
 public:
-    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<int> indegree(numCourses, 0);
-        vector<vector<int>> adj(numCourses);
+    vector<int> findOrder(int v, vector<vector<int>>& prerequisites) {
+        vector<int>indegree(v,0);
 
-        // Build the graph and the indegree array
-        for (auto& pre : prerequisites) {
+        vector<vector<int>>adj(v);
+
+        for(auto &pre : prerequisites){
             int prerequisite = pre[1];
             int course = pre[0];
 
@@ -18,36 +13,32 @@ public:
             indegree[course]++;
         }
 
-        queue<int> q;
+        queue<int>q;
 
-        // Push all courses with no prerequisites into the queue
-        for (int i = 0; i < numCourses; ++i) {
-            if (indegree[i] == 0) {
+        for(int i=0; i<v; i++){
+            if(indegree[i] == 0){
                 q.push(i);
             }
         }
 
-        vector<int> topoOrder;
-
-        // Process the courses
-        while (!q.empty()) {
-            int course = q.front();
+        vector<int>topo;
+        while(!q.empty()){
+            int node = q.front();
             q.pop();
-            topoOrder.push_back(course);
+            topo.push_back(node);
 
-            // For each dependent course, reduce its indegree
-            for (int next : adj[course]) {
-                indegree[next]--;
-                if (indegree[next] == 0) {
-                    q.push(next);
+            for(auto it: adj[node]){
+                indegree[it]--;
+                if(indegree[it] == 0){
+                    q.push(it);
                 }
             }
         }
-
-        // If we have processed all courses, return the order
-        if (topoOrder.size() == numCourses) {
-            return topoOrder;
+        if(topo.size() == v){
+            return topo;
         }
-        return {};
+        else {
+            return {};
+        }
     }
 };
