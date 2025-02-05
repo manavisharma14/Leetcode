7305da89c307cab4971ed588b07d4052c87540db
Manavi Sharma <75850906+manavisharma14@users.noreply.github.com>
2025-02-05 12:06:00 -0600

    Time: 3 ms (14.64%), Space: 18.6 MB (17.82%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -4,33 +4,32 @@ public:
         int m = image.size();
         int n = image[0].size();
 
+        vector<vector<int>>visited(m, vector<int>(n,0));
+
         queue<pair<int,int>>q;
-
-        q.push({sr,sc});
         int originalcolor = image[sr][sc];
-        
-        if (originalcolor == color) return image;
-
         image[sr][sc] = color;
+        q.push({sr, sc});
+        visited[sr][sc] = 1;
 
         int directions[4][2] = {{1,0}, {-1,0}, {0,1}, {0,-1}};
 
         while(!q.empty()){
-            auto node = q.front();
+            int row = q.front().first;
+            int col = q.front().second;
             q.pop();
 
-            int i = node.first;
-            int j = node.second;
+            for(int i=0; i<4; i++){
+                int dx = row + directions[i][0];
+                int dy = col + directions[i][1];
 
-            for(int k=0; k<4; k++){
-                int dx = i + directions[k][0];
-                int dy = j + directions[k][1];
-
-                if(dx>=0 && dx<m && dy>=0 && dy<n && image[dx][dy] == originalcolor){
-                    q.push({dx, dy});
+                if(dx>=0 && dx<m && dy>=0 && dy<n && image[dx][dy] == originalcolor && !visited[dx][dy]){
                     image[dx][dy] = color;
+                    q.push({dx,dy});
+                    visited[dx][dy] = 1;
                 }
             }
+
         }
         return image;
     }
