aef8a9053fe071b875125a2f91bd324f88d655c8
Manavi Sharma <75850906+manavisharma14@users.noreply.github.com>
2025-02-24 16:53:50 -0600

    Time: 3 ms (96.84%), Space: 31.8 MB (74.34%) - LeetHub

--- 1073-number-of-enclaves/1073-number-of-enclaves.cpp
+++ 1073-number-of-enclaves/1073-number-of-enclaves.cpp
@@ -1,23 +1,31 @@
 class Solution {
 public:
     int numEnclaves(vector<vector<int>>& grid) {
+        // cal no of enclosed land cells
+
         int m = grid.size();
         int n = grid[0].size();
+
         vector<vector<int>>visited(m, vector<int>(n,0));
         queue<pair<int,int>>q;
-
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-                if(i==0 || i==m-1 || j==0 || j==n-1){
-                    if(grid[i][j] == 1){
-                        q.push({i,j});
-                        visited[i][j] = 1;
-                    }
-                }
-            }
+        
+        // travserse the first and last row
+        for(int j=0; j<n; j++){
+            if(grid[0][j] == 1) q.push({0,j});
+            visited[0][j] = 1;
+            if(grid[m-1][j] == 1) q.push({m-1, j});
+            visited[m-1][j] = 1;
         }
 
-        int directions[4][2] = {{1,0}, {-1,0}, {0,1}, {0,-1}};
+        // traverse the first and last col
+        for(int i=0; i<m; i++){
+            if(grid[i][0] == 1) q.push({i,0});
+            visited[i][0] = 1;
+            if(grid[i][n-1] == 1) q.push({i,n-1});
+            visited[i][n-1] = 1;
+        }
+
+        int directions[4][2] = {{1,0},{-1,0},{0,1},{0,-1}};
 
         while(!q.empty()){
             int row = q.front().first;
@@ -25,19 +33,18 @@ public:
             q.pop();
 
             for(int i=0; i<4; i++){
-                int dx = row + directions[i][0];
-                int dy = col + directions[i][1];
+                int newr = row + directions[i][0];
+                int newc = col + directions[i][1];
 
-                if(dx>=0 && dx<m && dy>=0 && dy<n && grid[dx][dy] == 1 && !visited[dx][dy]){
-                    q.push({dx,dy});
-                    visited[dx][dy] = 1;
+                if(newr>=0 && newr<m && newc>=0 && newc<n && grid[newr][newc] == 1 && !visited[newr][newc]){
+                    visited[newr][newc] = 1;
+                    q.push({newr, newc});
                 }
             }
+
         }
-
-        int count=0; 
-
-        for(int i=0 ;i<m; i++){
+        int count = 0;
+        for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
                 if(grid[i][j] == 1 && !visited[i][j]){
                     count++;
