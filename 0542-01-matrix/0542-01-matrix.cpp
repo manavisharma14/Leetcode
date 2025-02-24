385060107952f635afbcfdbf89b0f1724db96a8f
Manavi Sharma <75850906+manavisharma14@users.noreply.github.com>
2025-02-24 14:30:19 -0600

    Time: 7 ms (93.57%), Space: 38.8 MB (42.42%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -9,37 +9,38 @@ public:
 
         queue<pair<pair<int,int>,int>>q;
 
-        for(int i=0 ;i<m; i++){
+        for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
                 if(mat[i][j] == 0){
-                    q.push({{i , j} , 0});
+                    q.push({{i,j}, 0});
                     distance[i][j] = 0;
                     visited[i][j] = 1;
+
                 }
             }
         }
-        
+
         int directions[4][2] = {{1,0}, {-1,0}, {0,1}, {0,-1}};
 
         while(!q.empty()){
-            auto row = q.front().first.first;
+            int row = q.front().first.first;
             int col = q.front().first.second;
-            int steps = q.front().second;
+            int dis = q.front().second;
             q.pop();
 
-            distance[row][col] = steps;
-
             for(int i=0; i<4; i++){
-                int dx = row + directions[i][0];
-                int dy = col + directions[i][1];
+                int newr = row + directions[i][0];
+                int newc = col + directions[i][1];
 
-                if(dx>=0 && dx<m && dy>=0 && dy<n && !visited[dx][dy]){
-                    visited[dx][dy] = 1;
-                    q.push({{dx, dy}, steps+1});
+                if(newr>=0 && newr<m && newc>=0 && newc<n && !visited[newr][newc]){
+                    visited[newr][newc] = 1;
+                    distance[newr][newc] = dis + 1;
+                    q.push({{newr, newc}, dis+1});
                 }
             }
-        }
-        return distance;    
+        } 
+
+    return distance;
 
     }
 };
