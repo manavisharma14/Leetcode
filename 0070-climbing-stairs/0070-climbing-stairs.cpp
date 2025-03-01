1a04006012bc4276e1a3a3a15201e1ab6238572f
Manavi Sharma <75850906+manavisharma14@users.noreply.github.com>
2025-03-01 11:50:02 -0600

    Time: 0 ms (100%), Space: 8.5 MB (34.53%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -3,14 +3,16 @@ public:
     int climbStairs(int n) {
       
         if(n<=1) return n;
-        int prev2 = 1;
-        int prev1 = 1;
 
-        for(int i=2; i<=n; i++){
-            int curr = prev2 + prev1;
-            prev2 = prev1;
-            prev1 = curr;
+        vector<int>dp(n+1, -1);
+        dp[1] = 1;
+        dp[2] = 2; 
+
+        if(dp[n] != -1) return dp[n];
+
+        for(int i=3; i<=n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
         }
-        return prev1;
+        return dp[n];
     }
 };
