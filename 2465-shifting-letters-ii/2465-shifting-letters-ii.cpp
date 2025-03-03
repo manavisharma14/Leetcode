fb646624b81a0b5eceabc3376204aaf3e4207d3b
Manavi Sharma <75850906+manavisharma14@users.noreply.github.com>
2025-03-03 11:12:15 -0600

    Time: 7 ms (51.51%), Space: 97.8 MB (67.07%) - LeetHub

--- 2465-shifting-letters-ii/2465-shifting-letters-ii.cpp
+++ 2465-shifting-letters-ii/2465-shifting-letters-ii.cpp
@@ -1,25 +1,30 @@
 class Solution {
 public:
     string shiftingLetters(string s, vector<vector<int>>& shifts) {
-        int n = s.size();
-        vector<int> shift(n + 1, 0);
 
-        // Process the shifts
-        for (auto& shiftOp : shifts) {
-            int start = shiftOp[0], end = shiftOp[1], direction = shiftOp[2];
-            shift[start] += (direction == 1 ? 1 : -1);
-            shift[end + 1] -= (direction == 1 ? 1 : -1);
+        int n = s.length();
+        vector<int>prefixsum(n+1, 0);
+        for(auto& shift: shifts){
+            int start = shift[0];
+            int end = shift[1];
+            int direction = shift[2];
+
+            if (direction == 0) { // Left shift
+                prefixsum[start] -= 1;
+                prefixsum[end + 1] += 1;
+            } 
+            else { // Right shift
+                prefixsum[start] += 1;
+                prefixsum[end + 1] -= 1;
+            }
         }
 
-        int currentShift = 0;
-        for (int i = 0; i < n; ++i) {
-            currentShift += shift[i];
-            shift[i] = currentShift;
-        }
-
-        for (int i = 0; i < n; ++i) {
-            int netShift = (shift[i] % 26 + 26) % 26;
-            s[i] = 'a' + (s[i] - 'a' + netShift) % 26;
+        int shiftvalue=0;
+        for(int i=0; i<n; i++){
+            shiftvalue += prefixsum[i];
+            int newchar = ((s[i] - 'a') + shiftvalue)%26;
+            if(newchar < 0) newchar += 26;
+            s[i] = 'a' + newchar;
         }
 
         return s;
