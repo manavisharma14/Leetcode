6f2f2f9f006e97e512912673d220f1de0d442c49
Manavi Sharma <75850906+manavisharma14@users.noreply.github.com>
2025-03-03 15:15:20 -0600

    Time: 325 ms (12.32%), Space: 166.2 MB (13.17%) - LeetHub

--- 0952-word-subsets/0952-word-subsets.cpp
+++ 0952-word-subsets/0952-word-subsets.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
     vector<string> wordSubsets(vector<string>& words1, vector<string>& words2) {
-        unordered_map<char, int> maxfreq;
+        unordered_map<char,int>maxfreq;
         vector<string>ans;
-
 
         for(const string& word: words2){
             unordered_map<char,int>freq;
             for(char c: word){
                 freq[c]++;
             }
-            for(auto& [c,count] : freq){
+            for(auto &[c, count] : freq){
                 maxfreq[c] = max(maxfreq[c], count);
             }
         }
 
-        vector<string>result;
 
         for(const string& word: words1){
             unordered_map<char,int>freq;
@@ -23,19 +21,20 @@ public:
                 freq[c]++;
             }
 
+
             bool isuniversal = true;
-            for(auto& [c, requiredfreq] : maxfreq){
+            for(auto &[c, requiredfreq] : maxfreq){
                 if(freq[c] < requiredfreq){
                     isuniversal = false;
+                    break;
                 }
             }
+
             if(isuniversal){
                 ans.push_back(word);
             }
         }
-
         return ans;
-
 
     }
 };
