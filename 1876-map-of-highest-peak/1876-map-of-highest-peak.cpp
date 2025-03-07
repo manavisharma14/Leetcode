b72925f7cded740c035e64eea6d536486be32860
Manavi Sharma <75850906+manavisharma14@users.noreply.github.com>
2025-03-07 14:18:26 -0600

    Time: 70 ms (61.6%), Space: 112.8 MB (67.9%) - LeetHub

--- 1876-map-of-highest-peak/1876-map-of-highest-peak.cpp
+++ 1876-map-of-highest-peak/1876-map-of-highest-peak.cpp
@@ -3,44 +3,49 @@ public:
 
     
     vector<vector<int>> highestPeak(vector<vector<int>>& isWater) {
+
         int rows = isWater.size();
         int cols = isWater[0].size();
 
         vector<vector<int>>grid(rows, vector<int>(cols, -1));
 
         for(int i=0; i<rows; i++){
-            for (int j=0; j<cols; j++){
+            for(int j=0; j<cols; j++){
                 if(isWater[i][j] == 1){
                     grid[i][j] = 0;
                 }
             }
         }
 
+        int directions[4][2] = {{1,0}, {-1,0}, {0,1}, {0,-1}};
+
         queue<pair<int,int>>q;
 
         for(int i=0; i<rows; i++){
-            for (int j=0; j<cols; j++){
+            for(int j=0; j<cols; j++){
                 if(grid[i][j] == 0){
-                q.push({i,j});
-            }
-        }
-        }
-
-        int directions[4][2] = {{1,0}, {-1,0}, {0,1}, {0,-1}};
-
-        while(!q.empty()){
-            auto [x, y] = q.front(); q.pop();
-
-            for(auto [dx, dy] : directions){
-                int nx = x + dx;
-                int ny = y + dy;
-
-                if(nx >= 0 && nx < rows && ny >=0 && ny < cols && grid[nx][ny] == -1){
-                    grid[nx][ny] = grid[x][y] + 1;
-                    q.push({nx, ny});
+                    q.push({i,j});
                 }
             }
         }
+
+        while(!q.empty()){
+            int row = q.front().first;
+            int col = q.front().second;
+            q.pop();
+
+            for(int i=0; i<4; i++){
+                int newr = row + directions[i][0];
+                int newc = col + directions[i][1];
+
+                if(newr>=0 && newr<rows && newc>=0 && newc<cols && grid[newr][newc] == -1){
+                    grid[newr][newc] = grid[row][col] + 1;
+                    q.push({newr, newc});
+                }
+            }
+        }
+
+        
 return grid;
     }
 };
