be8864bd7a50f971e01034dea0ae4709f2cb592f
Manavi Sharma <75850906+manavisharma14@users.noreply.github.com>
2025-03-06 20:27:57 -0600

    Time: 0 ms (100%), Space: 66.8 MB (45.81%) - LeetHub

--- 2145-grid-game/2145-grid-game.cpp
+++ 2145-grid-game/2145-grid-game.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
     long long gridGame(vector<vector<int>>& grid) {
+
         long long top_sum = 0;
         long long bottom_sum = 0;
 
-        for (int j = 0; j < grid[0].size(); j++) {
-            top_sum += grid[0][j];
+        for(int i=0; i<grid[0].size(); i++){
+            top_sum += grid[0][i];
         }
 
         long long result = LLONG_MAX; 
@@ -15,6 +16,10 @@ public:
             result = min(result, max(top_sum, bottom_sum));
             bottom_sum += grid[1][i];
         }
+
+
+
+        
         return result;
     }
 };
