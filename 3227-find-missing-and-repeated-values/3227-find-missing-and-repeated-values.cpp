a8f52514b8ffbab8b96331eb8da3b27ba867da3f
Manavi Sharma <75850906+manavisharma14@users.noreply.github.com>
2025-03-28 18:27:13 -0500

    Time: 4 ms (47.49%), Space: 26.9 MB (44.78%) - LeetHub

--- 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.cpp
+++ 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.cpp
@@ -4,24 +4,25 @@ public:
         int n = grid.size();
         int N = n*n;
         vector<int>freq(N+1,0);
+
         int missing = -1;
         int repeated = -1;
 
-        for(auto& row : grid){
-            for(int num: row){
+        for(int i=0; i<n; i++){
+            for(int num : grid[i]){
                 freq[num]++;
             }
         }
 
         for(int i=1; i<=N; i++){
-            if(freq[i] == 2){
-                repeated = i;
-            }
-
             if(freq[i] == 0){
                 missing = i;
             }
+            if(freq[i] == 2){
+                repeated = i;
+            }
         }
         return {repeated, missing};
+
     }
 };
