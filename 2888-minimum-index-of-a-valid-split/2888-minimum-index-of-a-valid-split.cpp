4fa83da328e521153bd49d8cbaa9c7725f2779e7
Manavi Sharma <75850906+manavisharma14@users.noreply.github.com>
2025-03-28 20:11:12 -0500

    Time: 32 ms (55.26%), Space: 98.6 MB (41.57%) - LeetHub

--- 2888-minimum-index-of-a-valid-split/2888-minimum-index-of-a-valid-split.cpp
+++ 2888-minimum-index-of-a-valid-split/2888-minimum-index-of-a-valid-split.cpp
@@ -3,19 +3,22 @@ public:
     int minimumIndex(vector<int>& nums) {
 
         unordered_map<int,int>freq;
+
+        int dominant = -1;
+
         for(int num : nums){
             freq[num]++;
         }
 
         int n = nums.size();
-        int dominant = -1;
 
-        for(auto &[num, count]: freq){
+        for(auto &[num, count] : freq){
             if(count*2 > n){
                 dominant = num;
                 break;
             }
         }
+
 
         int leftcount = 0;
         int totalcount = freq[dominant];
@@ -25,15 +28,14 @@ public:
                 leftcount++;
             }
 
-            int leftsize = i+1;
-            int rightsize = n-leftsize;
             int rightcount = totalcount - leftcount;
+            int leftsize = i+1;
+            int rightsize = n - leftsize;
 
             if(leftcount*2 > leftsize && rightcount*2 > rightsize){
                 return i;
             }
         }
-
-    return -1;
+        return -1;
     }
 };
