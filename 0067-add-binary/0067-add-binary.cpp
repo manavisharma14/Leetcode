f4792b586434f52cfecbd20ed181833dbf8b5ff7
Manavi Sharma <75850906+manavisharma14@users.noreply.github.com>
2025-04-02 11:33:18 -0500

    Time: 0 ms (100%), Space: 9 MB (65.1%) - LeetHub

--- 0067-add-binary/0067-add-binary.cpp
+++ 0067-add-binary/0067-add-binary.cpp
@@ -3,29 +3,30 @@ public:
     string addBinary(string a, string b) {
         int n1 = a.length();
         int n2 = b.length();
-        int carry = 0;
-        int i=n1-1, j=n2-1;
         string result = "";
 
+        int i=n1-1;
+        int j=n2-1;
+        int carry = 0;
+
         while(i>=0 && j>=0){
-            int sum = (a[i]-'0' + b[j]-'0' + carry);
-            result += (sum % 2) + '0';
+            int sum = (a[i]-'0' + b[j]-'0' + carry) ;
+            result += sum%2 + '0';
             carry = sum/2;
-            i--; 
+            i--;
             j--;
         }
 
-        // remaining digits of a
         while(i>=0){
-            int sum = (a[i] - '0') + carry;
-            result += (sum % 2) + '0';
+            int sum = (a[i]-'0' + carry) ;
+            result += sum%2 + '0';
             carry = sum/2;
             i--;
         }
 
         while(j>=0){
-            int sum = (b[j] - '0') + carry;
-            result += (sum % 2) + '0';
+            int sum = (b[j]-'0' + carry) ;
+            result += sum%2 + '0';
             carry = sum/2;
             j--;
         }
@@ -34,10 +35,9 @@ public:
             result += '1';
         }
 
-
         reverse(result.begin(), result.end());
-        return result;
 
+        return result;
 
     }
 };
