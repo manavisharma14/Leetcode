71a6f38654617bc25dad172264c4525b1b12909f
Manavi Sharma <75850906+manavisharma14@users.noreply.github.com>
2025-04-14 00:34:14 -0500

    Time: 0 ms (100%), Space: 10.9 MB (21.22%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,21 +1,33 @@
 class Solution {
 public:
-    int roblinear(vector<int>& nums, int start, int end){
-        if(start>end) return 0;
-        int prev = nums[start];
-        int prev2 = 0;
 
-        for(int i=start+1; i<=end; i++){
-            int curr = max(prev, prev2+nums[i]);
-            prev2 = prev;
-            prev = curr;
-        }
-        return prev;
+    int func(vector<int>& nums, int n, int idx, int start, vector<int>& dp){
+        if(idx<start) return 0;
+        if(idx == start) return nums[start];
+
+        if(dp[idx] != -1) return dp[idx];
+
+        int pick = func(nums, n, idx-2, start, dp) + nums[idx];
+        int nonpick = func(nums, n, idx-1, start, dp);
+
+        dp[idx] = max(pick, nonpick);
+        return dp[idx];
+
     }
+
     int rob(vector<int>& nums) {
         int n = nums.size();
+        
+        if(n==0) return 0;
         if(n==1) return nums[0];
 
-        return max(roblinear(nums,0,n-2), roblinear(nums,1,n-1));
+        vector<int> dp1(n, -1);
+        vector<int> dp2(n, -1);
+
+        
+        int ans1 = func(nums, n, n-2, 0, dp1);
+        int ans2 = func(nums, n, n-1, 1, dp2);
+
+        return max(ans1, ans2);
     }
 };
