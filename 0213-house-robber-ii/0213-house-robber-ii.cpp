0c32d449fc92149ef466c980f42c7c0ab663d89d
Manavi Sharma <75850906+manavisharma14@users.noreply.github.com>
2025-04-14 19:43:18 -0500

    Time: 0 ms (100%), Space: 11 MB (9.05%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,33 +1,40 @@
 class Solution {
 public:
 
-    int func(vector<int>& nums, int n, int idx, int start, vector<int>& dp){
-        if(idx<start) return 0;
-        if(idx == start) return nums[start];
+    int func(int idx, int n, vector<int>& nums, vector<int>& dp){
+        if(idx>n) return 0;
+
+        //pick
 
         if(dp[idx] != -1) return dp[idx];
 
-        int pick = func(nums, n, idx-2, start, dp) + nums[idx];
-        int nonpick = func(nums, n, idx-1, start, dp);
+        int pick = func(idx+2, n, nums, dp) + nums[idx];
+        int nonpick = func(idx+1, n, nums, dp);
 
-        dp[idx] = max(pick, nonpick);
-        return dp[idx];
+        return dp[idx] = max(pick, nonpick);
 
     }
 
+
     int rob(vector<int>& nums) {
         int n = nums.size();
-        
+
         if(n==0) return 0;
+
         if(n==1) return nums[0];
+        if(n==2) return max(nums[0], nums[1]);
 
-        vector<int> dp1(n, -1);
-        vector<int> dp2(n, -1);
+        vector<int>dp1(n, -1);
+        vector<int>dp2(n,-1);
 
-        
-        int ans1 = func(nums, n, n-2, 0, dp1);
-        int ans2 = func(nums, n, n-1, 1, dp2);
+
+        // ans1 0 to n-2
+        int ans1 = func(0, n-2, nums, dp1);
+        // ans2 1 to n-1
+        int ans2 = func(1, n-1, nums, dp2);
 
         return max(ans1, ans2);
+        
+        
     }
 };
