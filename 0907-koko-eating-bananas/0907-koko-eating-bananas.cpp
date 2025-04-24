7f2a10c2e1399cff5259d5e1485a1a55067c0d06
Manavi Sharma <75850906+manavisharma14@users.noreply.github.com>
2025-04-24 18:34:56 -0500

    Time: 6 ms (80.2%), Space: 22.9 MB (74.21%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
@@ -1,44 +1,40 @@
 class Solution {
 public:
-    int findmax(vector<int>& piles) {
+
+    int findmax(vector<int>& piles){
         int maxi = INT_MIN;
-        for (int i = 0; i < piles.size(); i++) {
+        for(int i=0; i<piles.size(); i++){
             maxi = max(maxi, piles[i]);
         }
         return maxi;
     }
 
-    long long func(vector<int>& piles, int hourly) {
+    long long func(vector<int>&piles, int hourly){
+        int n = piles.size();
         long long totalhours = 0;
-
-
-        for (int i = 0; i < piles.size(); i++) {
-            totalhours += (piles[i] + hourly - 1) / hourly;  
+        for(int i=0; i<n; i++){
+            totalhours += (piles[i] + hourly - 1)/hourly;
         }
         return totalhours;
+
     }
-
-
+    
     int minEatingSpeed(vector<int>& piles, int h) {
-
         int low = 1;
         int high = findmax(piles);
 
+        while(low<=high){
+            int mid = low + ((high-low) >> 1);
+            long long total = func(piles, mid);
 
-        while (low <= high) {
-            int mid = low + (high - low) / 2;  
-            long long total = func(piles, mid);  
-
-
-            if (total <= h) {
-                high = mid - 1;  
+            if(total <= h){
+                high = mid-1;
+            }
+            else{
+                low = mid+1;
             }
 
-            else {
-                low = mid + 1; 
-            }
         }
-
-        return low;  // Return the minimum valid speed
+        return low;
     }
 };
