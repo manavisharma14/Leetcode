09e6d6de8d7dc68a978334cf76f91197a0fd9feb
Manavi Sharma <75850906+manavisharma14@users.noreply.github.com>
2025-04-24 00:24:27 -0500

    Time: 35 ms (89.91%), Space: 45.5 MB (43.48%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -2,21 +2,24 @@ class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
         int n = nums.size();
+        unordered_map<int, int> map;  // Stores the frequency of prefix sums
+        int sum = 0;  // Current prefix sum
+        int count = 0;
 
-        unordered_map<int,int>map;
-        map[0]=1;
-        
-        int current_sum = 0, count=0; 
+        map[0] = 1;  // To handle the case where the subarray sum is exactly `k` from the start
 
-        for(int num : nums){
-            current_sum += num;
+        for (int i = 0; i < n; i++) {
+            sum += nums[i];  // Update the running sum
 
-            if(map.find(current_sum - k) != map.end()){
-                count += map[current_sum - k];
+            // If sum == k, we have found a valid subarray from the beginning to the current index
+            if (map.find(sum - k) != map.end()) {
+                count += map[sum - k];  // Add how many times `sum - k` has occurred before
             }
 
-            map[current_sum]++;
+            // Record the current prefix sum
+            map[sum]++;
         }
+
         return count;
     }
 };
