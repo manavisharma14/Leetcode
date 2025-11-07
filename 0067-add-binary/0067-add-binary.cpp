e76c9798c45c51adfe87da13dae4257ae906c4f9
Manavi Sharma <75850906+manavisharma14@users.noreply.github.com>
2025-11-07 17:10:52 -0600

    Time: 3 ms (25.07%), Space: 11 MB (19.94%) - LeetHub

--- 0067-add-binary/0067-add-binary.cpp
+++ 0067-add-binary/0067-add-binary.cpp
@@ -1,43 +1,28 @@
 class Solution {
 public:
     string addBinary(string a, string b) {
-        int n1 = a.length();
-        int n2 = b.length();
-        string result = "";
-
-        int i=n1-1;
-        int j=n2-1;
+        int sum = 0;
         int carry = 0;
+        string res= "";
 
-        while(i>=0 && j>=0){
-            int sum = (a[i]-'0' + b[j]-'0' + carry) ;
-            result += sum%2 + '0';
-            carry = sum/2;
+        int i = a.length()-1;
+        int j = b.length()-1;
+
+        while(i>=0 || j>=0 || carry){
+            int bitA = (i>=0)? a[i] -'0' : 0;
+            int bitB = (j>=0)? b[j]-'0' : 0;
+
+            sum = bitA ^ bitB ^ carry;
+            
+            carry = (bitA & bitB) | (bitA & carry) | (bitB & carry);
+
+            res = char(sum + '0') + res;
             i--;
             j--;
         }
 
-        while(i>=0){
-            int sum = (a[i]-'0' + carry) ;
-            result += sum%2 + '0';
-            carry = sum/2;
-            i--;
-        }
-
-        while(j>=0){
-            int sum = (b[j]-'0' + carry) ;
-            result += sum%2 + '0';
-            carry = sum/2;
-            j--;
-        }
-
-        if(carry){
-            result += '1';
-        }
-
-        reverse(result.begin(), result.end());
-
-        return result;
-
+        
+        return res;
+      
     }
 };
