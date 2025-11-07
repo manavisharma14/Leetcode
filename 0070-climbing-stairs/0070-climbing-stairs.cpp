daf191af6139bfcf569ef48216c320109f975c1a
Manavi Sharma <75850906+manavisharma14@users.noreply.github.com>
2025-11-07 10:10:01 -0600

    Time: 2 ms (5.1%), Space: 8.7 MB (17.49%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
-
     int func(vector<int>&dp, int n){
         if(n==0 || n==1) return dp[n];
+
         if(dp[n] != -1) return dp[n];
         dp[n] = func(dp, n-1) + func(dp, n-2);
         return dp[n];
     }
+
     int climbStairs(int n) {
-        vector<int>dp(n+1, -1);
-        
+        vector<int>dp(n+1,-1);
+
         dp[0] = 1;
         dp[1] = 1;
+
         return func(dp, n);
     }
 };
