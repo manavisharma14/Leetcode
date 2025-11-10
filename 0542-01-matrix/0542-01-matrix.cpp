022271c5a545f874b53fe50ca8053821d47a9b9e
Manavi Sharma <75850906+manavisharma14@users.noreply.github.com>
2025-11-10 11:37:48 -0600

    Time: 23 ms (43.14%), Space: 36.8 MB (55.31%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,46 +1,40 @@
 class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        queue<pair<int,int>>q;
         int m = mat.size();
         int n = mat[0].size();
 
-        vector<vector<int>>distance(m, vector<int>(n,0));
-        vector<vector<int>>visited(m, vector<int>(n,0));
+        vector<vector<bool>>visited(m, vector<bool>(n, false));
 
-        queue<pair<pair<int,int>,int>>q;
+        vector<vector<int>>result(m, vector<int>(n,0));
 
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
                 if(mat[i][j] == 0){
-                    q.push({{i,j}, 0});
-                    distance[i][j] = 0;
-                    visited[i][j] = 1;
-
+                    q.push({i,j});
+                    visited[i][j] = true;
                 }
             }
         }
-
+        
         int directions[4][2] = {{1,0}, {-1,0}, {0,1}, {0,-1}};
-
         while(!q.empty()){
-            int row = q.front().first.first;
-            int col = q.front().first.second;
-            int dis = q.front().second;
-            q.pop();
+            auto [r,c] = q.front(); q.pop();
 
-            for(int i=0; i<4; i++){
-                int newr = row + directions[i][0];
-                int newc = col + directions[i][1];
+            for(auto [dr, dc] : directions){
+                int nrow = dr + r;
+                int ncol = dc + c;
 
-                if(newr>=0 && newr<m && newc>=0 && newc<n && !visited[newr][newc]){
-                    visited[newr][newc] = 1;
-                    distance[newr][newc] = dis + 1;
-                    q.push({{newr, newc}, dis+1});
+                if(nrow>=0 && nrow<m && ncol>=0 && ncol<n && !visited[nrow][ncol]){
+                    result[nrow][ncol] = result[r][c] + 1;
+                    visited[nrow][ncol] = true;
+                    q.push({nrow, ncol});
                 }
             }
-        } 
 
-    return distance;
-
+            
+        }
+        return result;
     }
 };
