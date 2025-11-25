8ab3d55d7d5bdf684e00fac90be60263ddb94e20
Manavi Sharma <75850906+manavisharma14@users.noreply.github.com>
2025-11-24 20:29:17 -0600

    Time: 0 ms (100%), Space: 8.9 MB (66.11%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -2,20 +2,25 @@ class Solution {
 public:
     bool isValid(string s) {
         std::stack<char>stk;
+        int n = s.length();
 
-        for(int i=0; i<s.length(); i++){
-            if(s[i] == '(' || s[i] == '{' || s[i] == '['){
+        for(int i=0; i<n; i++){
+            if(s[i] == '{' || s[i] == '[' || s[i] == '('){
                 stk.push(s[i]);
             }
-            else if( !stk.empty() && ((s[i] == '}' && stk.top() == '{') || 
-                (s[i] == ']' && stk.top() == '[') ||
-                (s[i] == ')' && stk.top() == '('))){
-                stk.pop();
-            } else{ 
-                return false;
+            if(s[i] == ']' || s[i] == '}'|| s[i] == ')'){
+                if(stk.empty()) return false;
+                if((s[i] == ']' && stk.top() == '[') ||
+                (s[i] == ')' && stk.top() == '(') || 
+                (s[i] == '}' && stk.top() == '{')){
+                    stk.pop();
+                } else {
+                    return false;
+                }
             }
-            
-        }
+                        
+            }
+        
         return stk.empty();
     }
 };
