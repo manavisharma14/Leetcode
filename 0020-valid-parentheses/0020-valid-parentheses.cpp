d7285a95d0868753083f1a570a3f6b8476f67f7f
Manavi Sharma <75850906+manavisharma14@users.noreply.github.com>
2025-11-25 14:04:58 -0600

    Time: 0 ms (100%), Space: 9 MB (37.87%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -5,22 +5,20 @@ public:
         int n = s.length();
 
         for(int i=0; i<n; i++){
-            if(s[i] == '{' || s[i] == '[' || s[i] == '('){
+            if(s[i] == '(' || s[i] == '[' || s[i] == '{'){
                 stk.push(s[i]);
-            }
-            if(s[i] == ']' || s[i] == '}'|| s[i] == ')'){
-                if(stk.empty()) return false;
-                if((s[i] == ']' && stk.top() == '[') ||
-                (s[i] == ')' && stk.top() == '(') || 
-                (s[i] == '}' && stk.top() == '{')){
-                    stk.pop();
-                } else {
-                    return false;
-                }
-            }
-                        
-            }
-        
+            } 
+            if(s[i] == ']' || s[i] == '}' || s[i] ==')'){
+                if(stk.empty()){ return false; }
+                else if(s[i] == ']' && stk.top() == '[' ||
+                    (s[i] == '}' && stk.top() == '{' ) || 
+                    s[i] == ')' && stk.top() == '('  ){
+                        stk.pop();
+                    } else {
+                        return false;
+                    }
+            } 
+        }
         return stk.empty();
     }
 };
