1283e1829c297184ca2f8f1a872e198e03390d20
Manavi Sharma <75850906+manavisharma14@users.noreply.github.com>
2025-11-25 22:29:47 -0600

    Time: 0 ms (100%), Space: 18.5 MB (16.11%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,32 +1,34 @@
 class Solution {
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        queue<pair<int,int>>q;
+        int originalColor = image[sr][sc];
         int m = image.size();
         int n = image[0].size();
 
         vector<vector<bool>>visited(m, vector<bool>(n, false));
 
-        std::queue<pair<int,int>>q;
-        q.push({sr,sc});
-        int originalcolor = image[sr][sc];
-
+        q.push({sr, sc});
         image[sr][sc] = color;
+        visited[sr][sc] = true;
 
+        vector<pair<int,int>> directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
         while(!q.empty()){
-            auto [r,c] = q.front(); q.pop();
+            auto [row, col] = q.front();
+            q.pop();
 
-            vector<pair<int,int>>directions = {{-1,0}, {1,0}, {0,-1}, {0,1}};
-            for(auto [dr,dc] : directions){
-                int nr = r + dr;
-                int nc = c + dc;
+            for(auto &[dx, dc] : directions){
+                int nx = dx+row;
+                int nc = dc+col;
 
-                if(nr>=0 && nr<m && nc>=0 && nc<n && image[nr][nc] == originalcolor && !visited[nr][nc]){
-                    image[nr][nc] = color;
-                    visited[nr][nc] = true;
-                    q.push({nr,nc});
-                }
+                if(nx>=0 && nx<m && nc>=0 && nc<n && !visited[nx][nc] && image[nx][nc] == originalColor){
+                image[nx][nc] = color;
+                visited[nx][nc] = true;
+                q.push({ nx, nc });
+            }
             }
         }
         return image;
+
     }
 };
