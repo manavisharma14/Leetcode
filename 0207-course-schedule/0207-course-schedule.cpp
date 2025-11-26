77d56760162d9b0778c5d062463ffe4eae334f2d
Manavi Sharma <75850906+manavisharma14@users.noreply.github.com>
2025-11-26 00:06:59 -0600

    Time: 11 ms (23.43%), Space: 19.3 MB (71.52%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -3,9 +3,9 @@ public:
 
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         int n = numCourses;
-        vector<vector<int>>adj(n);
-        vector<int>indegree(n,0);
 
+        vector<int>indegree(numCourses);
+        vector<vector<int>>adj(n);
         for(auto &pre: prerequisites){
             adj[pre[1]].push_back(pre[0]);
         }
@@ -15,26 +15,26 @@ public:
                 indegree[v]++;
             }
         }
-
         queue<int>q;
-        for(int i=0; i<n; i++){
+        for(int i=0 ;i<n; i++){
             if(indegree[i] == 0){
                 q.push(i);
             }
         }
-
         int count = 0;
         while(!q.empty()){
             int u = q.front();
             q.pop();
             count++;
 
-            for(int v : adj[u]){
+            for(int v: adj[u]){
                 indegree[v]--;
-                if(indegree[v] == 0) q.push(v);
+                if(indegree[v] == 0){
+                    q.push(v);
+                }
             }
         }
-        return count == numCourses;
+        return numCourses == count;
 
     }
 };
