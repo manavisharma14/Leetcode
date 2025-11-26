8b7b87157fc773fb6347019984d40e98b662af8f
Manavi Sharma <75850906+manavisharma14@users.noreply.github.com>
2025-11-25 23:13:12 -0600

    Time: 2 ms (82.24%), Space: 19.4 MB (59.83%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,32 +1,28 @@
 class Solution {
 public:
-    bool dfs( int node,  vector<int>&visited, vector<vector<int>>&adj){
-        visited[node] = 1;
 
-        for(int neighbour: adj[node]){
-            if(visited[neighbour] == 0){
-                if(dfs(neighbour, visited, adj)) return true;
-            } else if(visited[neighbour] == 1){
-                return true;
-            }
+    bool dfs(int n, vector<int>&visited, vector<vector<int>>&adj){
+        visited[n] = 1;
+        for(int neighbour : adj[n]){
+            if(visited[neighbour] == 1) { return true; }
+            if(visited[neighbour] == 0 && dfs(neighbour, visited, adj)) return true;
         }
-        visited[node] = 2;
+        visited[n] = 2;
         return false;
     }
 
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+
         vector<int>visited(numCourses, 0);
         vector<vector<int>>adj(numCourses);
 
-        for(auto &pre: prerequisites){
+        for( auto &pre: prerequisites){
             adj[pre[1]].push_back(pre[0]);
         }
 
-        for(int i=0; i<numCourses; i++){
-            if(visited[i] == 0){
-                if(dfs(i, visited, adj)) return false;
-            }
+        for(int i=0 ;i<numCourses; i++){
+            if(dfs(i, visited, adj)) return false;
         }
-        return true;
+    return true;
     }
 };
