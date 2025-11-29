ade505abab82ed300770b2b4483ba173022fb82e
Manavi Sharma <75850906+manavisharma14@users.noreply.github.com>
2025-11-28 21:34:40 -0600

    Time: 0 ms (100%), Space: 11.8 MB (84.84%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -9,17 +9,20 @@ public:
         }
 
         for(int j=0; j<minprelen; j++){
-            int c = strs[0][j];
+            char c = strs[0][j];
             bool match = true;
+
             for(int i=1; i<strs.size(); i++){
-                if(c != strs[i][j]){
+                if(strs[i][j] != c){
                     match = false;
+                    break;
                 }
             }
-
             if(match){
-                res+=c;
-            } else break;
+                res += c;
+            } else {
+                break;
+            }
         }
         return res;
     }
