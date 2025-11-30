3e18134e615b965bd82303796eff50c44ddfccc7
Manavi Sharma <75850906+manavisharma14@users.noreply.github.com>
2025-11-30 13:26:59 -0600

    Time: 7 ms (44.96%), Space: 11 MB (18.84%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     int characterReplacement(string s, int k) {
         int left = 0;
-        int right = 0;
         int n = s.length();
         unordered_map<int,int>mp;
         int maxlen = 0;
@@ -10,7 +9,7 @@ public:
 
         for(int right=0; right<n; right++){
             mp[s[right] - 'A']++;
-            maxfreq = max(maxfreq, mp[s[right] - 'A']);
+            maxfreq = max(maxfreq, mp[s[right]-'A']);
             while((right-left+1) - maxfreq > k){
                 mp[s[left] - 'A']--;
                 left++;
@@ -18,5 +17,6 @@ public:
             maxlen = max(maxlen, right-left+1);
         }
         return maxlen;
+
     }
 };
