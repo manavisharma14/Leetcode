435e0c6042f01d907c19e3c91c8c46a18f34a674
Manavi Sharma <75850906+manavisharma14@users.noreply.github.com>
2025-12-01 11:14:23 -0600

    Time: 2 ms (15.53%), Space: 10.3 MB (5.62%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
     bool isValid(string s) {
-        std::stack<char>stk;
-        int n = s.length();
+        std::stack<int>stk;
 
-        for(int i=0; i<n; i++){
-            if(s[i] == '(' || s[i] == '[' || s[i] == '{'){
+        for(int i=0; i<s.length(); i++){
+            if(s[i] == '(' || s[i] == '{' || s[i] == '['){
                 stk.push(s[i]);
-            } 
-            if(s[i] == ']' || s[i] == '}' || s[i] ==')'){
-                if(stk.empty()){ return false; }
-                else if(s[i] == ']' && stk.top() == '[' ||
-                    (s[i] == '}' && stk.top() == '{' ) || 
-                    s[i] == ')' && stk.top() == '('  ){
+            } else if(s[i] == '}' || s[i] == ']' || s[i] == ')'){
+                if(stk.empty()) return false;
+                if((s[i] == ')' && stk.top() == '(') || 
+                    (s[i] == ']' && stk.top() == '[') ||
+                    (s[i] == '}' && stk.top() == '{')){
                         stk.pop();
-                    } else {
-                        return false;
-                    }
-            } 
+                } else {
+                    return false;
+                }
+            }
         }
         return stk.empty();
     }
