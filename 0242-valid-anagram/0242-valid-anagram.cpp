7a7497a1e5c189f7f49a6b110255f3535a53d0d2
Manavi Sharma <75850906+manavisharma14@users.noreply.github.com>
2026-03-20 18:48:54 -0500

    Time: 0 ms (100%), Space: 9.9 MB (23.57%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-       unordered_map<char, int>mp;
-       if(s.size() != t.size()) return false;
+       unordered_map<char, int> freq;
+       if(s.length() != t.length()) return false;
 
        for(char c: s){
-        mp[c] += 1;
+        freq[c] += 1;
        }
 
        for(char c: t){
-        if(mp[c] == 0) return false;
-        mp[c]--;
-       } 
+        if(freq[c] == 0) {return false;}
+        freq[c]--;
+       }
        return true;
     }
 };
