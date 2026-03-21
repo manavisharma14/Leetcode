0f15e73bdb68d7b63f5f03052f33b58bae4eedf8
Manavi Sharma <75850906+manavisharma14@users.noreply.github.com>
2026-03-20 19:43:13 -0500

    Time: 4 ms (53.54%), Space: 14.7 MB (52.28%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -5,7 +5,7 @@ public:
         unordered_map<int,int>mp;
 
         for(int i=0; i<n; i++){
-            if(mp.find(target - nums[i]) != mp.end()){
+            if(mp.find(target - nums[i]) != mp.end()) { 
                 return {i, mp[target-nums[i]]};
             }
             mp[nums[i]] = i;
