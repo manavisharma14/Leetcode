15f0ba647e455917d74c6c2cd9fb558f26e75f1e
Manavi Sharma <75850906+manavisharma14@users.noreply.github.com>
2026-03-22 14:28:26 -0500

    Time: 0 ms (100%), Space: 42.1 MB (25.1%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -4,13 +4,13 @@ public:
         int n = nums.size();
         vector<int>prefix(n);
         vector<int>suffix(n);
-        vector<int>output(n);
+        vector<int>ans(n);
 
         prefix[0] = 1;
         suffix[n-1] = 1;
 
         for(int i=1; i<n; i++){
-            prefix[i] = prefix[i-1]*nums[i-1];
+            prefix[i] = nums[i-1]*prefix[i-1];
         }
 
         for(int i=n-2; i>=0; i--){
@@ -18,8 +18,8 @@ public:
         }
 
         for(int i=0; i<n; i++){
-            output[i] = prefix[i]*suffix[i];
+            ans[i] = prefix[i]*suffix[i];
         }
-        return output;
+        return ans;
     }
 };
