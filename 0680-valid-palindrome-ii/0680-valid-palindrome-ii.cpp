9318b25df48e97cf129eba64b7c7715f0486b88d
Manavi Sharma <75850906+manavisharma14@users.noreply.github.com>
2026-04-28 19:00:53 -0500

    Time: 7 ms (32.09%), Space: 26.9 MB (9.6%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
@@ -1,30 +1,29 @@
 class Solution {
 public:
-    
+
     bool check(int l, int r, string s){
-        int n = s.length();
         while(l<r){
-            if(s[r] != s[l]) return false;
+            if(s[l] != s[r]) return false;
             l++;
             r--;
         }
         return true;
     }
-
     bool validPalindrome(string s) {
         int n = s.length();
         int left = 0;
         int right = n-1;
 
-        while(left < right){
-            int skip = 0;
-            if(s[left] != s[right]){
-                if(check(left+1, right, s) || check(left, right-1, s)) return true;
+        while(left<right){
+            if(tolower(s[left]) != tolower(s[right])){
+                if(check(left+1, right, s)) return true;
+                else if(check(left, right-1, s)) return true;
                 else return false;
             }
             left++;
             right--;
         }
-        return true;
+        return true;   
     }
+
 };
