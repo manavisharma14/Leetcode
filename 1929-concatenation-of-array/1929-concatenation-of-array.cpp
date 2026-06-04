68110a5b409b40f3d484f5900f7b30686d170c7c
Manavi Sharma <75850906+manavisharma14@users.noreply.github.com>
2026-06-04 00:49:48 -0500

    Time: 0 ms (100%), Space: 16.8 MB (80.64%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.cpp
+++ 1929-concatenation-of-array/1929-concatenation-of-array.cpp
@@ -4,8 +4,11 @@ public:
         int n = nums.size();
         vector<int>ans(2*n);
 
-        for(int i=0; i<2*n; i++){
-            (i>n-1) ? ans[i] = nums[i-n] : ans[i] = nums[i];
+        for(int i=0; i<n; i++){
+            ans[i] = nums[i];
+            if(i<n){
+                ans[i+n] = nums[i];
+            }
         }
         return ans;
     }
