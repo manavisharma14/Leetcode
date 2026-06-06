d4c13178237e6418396808fed5e630fcddb89d95
Manavi Sharma <75850906+manavisharma14@users.noreply.github.com>
2026-06-05 19:05:53 -0500

    Time: 0 ms (100%), Space: 11.5 MB (77.67%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -3,23 +3,20 @@ public:
     void sortColors(vector<int>& nums) {
         int count0=0, count1=0, count2=0;
 
-        for(int i=0; i<nums.size(); i++){
-            if(nums[i] == 0) count0++;
-            if(nums[i] == 1) count1++;
-            if(nums[i] == 2) count2++;
+        for(int x: nums){
+            if(x==0) count0++;
+            else if(x==1) count1++;
+            else if(x==2) count2++;
         }
 
         for(int i=0; i<count0; i++){
             nums[i] = 0;
         }
-
-        for(int i=count0; i<count0 + count1; i++){
+        for(int i=count0; i<count0+count1; i++){
             nums[i] = 1;
         }
-
         for(int i=count0+count1; i<nums.size(); i++){
             nums[i] = 2;
         }
-        
     }
 };
