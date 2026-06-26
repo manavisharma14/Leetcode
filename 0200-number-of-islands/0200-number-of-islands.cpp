75d6616f17cbb2e5b636ac514a4ef0475b7e40eb
Manavi Sharma <75850906+manavisharma14@users.noreply.github.com>
2026-06-26 13:11:18 -0500

    Time: 28 ms (48.77%), Space: 19.1 MB (36.58%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -3,31 +3,33 @@ public:
     int numIslands(vector<vector<char>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
-        vector<vector<int>>visited(m, vector<int>(n,0));
-
-        
+        vector<vector<bool>>visited(m, vector<bool>(n, false));
         int islands = 0;
 
-        for(int i=0 ;i<m ; i++){
+        queue<pair<int,int>>q;
+
+        for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
-                if(grid[i][j] == '1' && visited[i][j] == 0){
+                if(grid[i][j] == '1' && !visited[i][j]){
                     islands++;
-                    queue<pair<int,int>>q;
                     q.push({i,j});
-                    visited[i][j] = 1;
+                    visited[i][j] = true;
+
+                    // bfs here
+                    // explore only connected ones
 
                     vector<pair<int,int>>directions = {{-1,0}, {1,0}, {0,-1}, {0,1}};
+
                     while(!q.empty()){
-                        auto [r,c] = q.front();
-                        q.pop();
+                        auto [r,c] = q.front(); q.pop();
 
-                        for(auto &[dr,dc] : directions){
-                            int nr = r+dr;
-                            int nc = c+dc;
+                        for(auto &[dr, dc] : directions){
+                            int nr = dr + r;
+                            int nc = dc + c;
 
-                            if(nr>=0 && nr<m && nc>=0 && nc<n && !visited[nr][nc] && grid[nr][nc] == '1'){
+                            if(nc>=0 && nc<n && nr>=0 && nr<m && grid[nr][nc] == '1' && !visited[nr][nc]){
                                 q.push({nr, nc});
-                                visited[nr][nc] = 1;
+                                visited[nr][nc] = true;
                             }
                         }
                     }
