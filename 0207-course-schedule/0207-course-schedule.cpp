70af7a8a830dcee83abe83aed64df2624587e8c2
Manavi Sharma <75850906+manavisharma14@users.noreply.github.com>
2026-06-26 20:14:38 -0500

    Time: 15 ms (9.75%), Space: 19.2 MB (81.13%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,39 +1,44 @@
 class Solution {
 public:
+    bool dfs(int i, vector<vector<int>>&adj, vector<int>&visited){
+        if(visited[i] == 1){
+            return false;
+        }
+
+        if(visited[i] == 2){
+            return true;
+        }
+        
+        visited[i] = 1;
+
+        for(int next: adj[i]){
+            if(!dfs(next, adj, visited)){
+                return false;
+            }
+        }
+        visited[i] = 2;
+        return true;
+    }
 
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<int>indegree(numCourses, 0);
         vector<vector<int>>adj(numCourses);
 
         for(auto &p: prerequisites){
             int course = p[0];
             int prerequisite = p[1];
-            
+
             adj[prerequisite].push_back(course);
-            indegree[course]++;
         }
 
-        queue<int>q;
+        vector<int>visited(numCourses, 0);
 
         for(int i=0; i<numCourses; i++){
-            if(indegree[i] == 0){
-                q.push(i);
+            if(visited[i] == 0){
+                if(!dfs(i, adj, visited)){
+                    return false;
+                }
             }
         }
-
-        int completed = 0;
-
-        while(!q.empty()){
-            int curr = q.front();
-            q.pop();
-
-            completed++;
-
-            for(int next: adj[curr]){
-                indegree[next]--;
-                if(indegree[next] == 0) q.push(next);
-            }
-        }
-        return completed == numCourses;
+        return true;
     }
 };
