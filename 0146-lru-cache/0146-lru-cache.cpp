040a43d581dc3f82c49cfa51917398aba9ad8475
Manavi Sharma <75850906+manavisharma14@users.noreply.github.com>
2026-06-27 12:06:39 -0500

    Time: 86 ms (46.8%), Space: 182.1 MB (32.99%) - LeetHub

---
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -0,0 +1,47 @@
+class LRUCache {
+private:
+    unordered_map<int, list<pair<int,int>>::iterator>mp;
+    list<pair<int,int>> cache;
+    int cap;
+
+public:
+    LRUCache(int capacity) {
+        cap = capacity;
+    }
+    
+    int get(int key) {
+        if(mp.find(key) == mp.end()){ return -1; }
+        
+        auto it = mp[key];
+        int value = it->second;
+
+        cache.erase(it);
+        cache.push_front({key, value});
+        mp[key] = cache.begin();
+
+        return value;
+    }
+    
+    void put(int key, int value) {
+        if(mp.find(key) != mp.end()){
+            auto it = mp[key];
+            cache.erase(it);
+        }
+
+        cache.push_front({key, value});
+        mp[key] = cache.begin();
+       
+        if(cache.size() > cap){
+            int lrukey = cache.back().first;
+            mp.erase(lrukey);
+            cache.pop_back();
+        }
+    }
+};
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
