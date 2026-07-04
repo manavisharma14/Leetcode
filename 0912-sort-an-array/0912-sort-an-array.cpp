cfc7b7e75007979f6e0d393337419eef109e2062
Manavi Sharma <75850906+manavisharma14@users.noreply.github.com>
2026-07-03 19:55:49 -0500

    Time: 47 ms (75.57%), Space: 79 MB (57.79%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -2,16 +2,16 @@ class Solution {
 public:
     vector<int> sortArray(vector<int>& nums) {
         priority_queue<int, vector<int>, greater<int>>pq;
-        vector<int>ans;
 
-        for(int x: nums){
-            pq.push(x);
+        for(int num: nums){
+            pq.push(num);
         }
+        
+        vector<int>result;
 
         while(!pq.empty()){
-            ans.push_back(pq.top());    
-            pq.pop();
+            result.push_back(pq.top()); pq.pop();
         }
-        return ans;
+        return result;
     }
 };
