9710673a191695859bc4240fcebf59d1fef4cbbf
Manavi Sharma <75850906+manavisharma14@users.noreply.github.com>
2026-07-05 19:54:00 -0500

    Time: 55 ms (28.68%), Space: 29.2 MB (30.5%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -10,14 +10,14 @@ public:
             int right = nums.size()-1;
 
             while(left < right){
-                int sum = nums[i] + nums[left] + nums[right];
+                int sum = nums[i]+nums[left]+nums[right];
                 if(sum == 0){
                     result.push_back({nums[i], nums[left], nums[right]});
-                    while(left<right && nums[left+1] == nums[left]) left++;
-                    while(left< right && nums[right-1] == nums[right]) right--;
+                    while(left<right && nums[left] == nums[left+1]) left++;
+                    while(left<right && nums[right] == nums[right-1]) right--;
                     left++;
                     right--;
-                }
+                } 
                 else if(sum > 0) right--;
                 else if(sum < 0) left++;
             }
