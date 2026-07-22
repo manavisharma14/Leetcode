061a85f2e5fd3051cd549c334b98ba315602c352
Manavi Sharma <75850906+manavisharma14@users.noreply.github.com>
2026-07-22 12:23:12 -0500

    Time: 0 ms (100%), Space: 12.4 MB (16.6%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -6,19 +6,17 @@ public:
         int p2 = n-1;
         int i = m+n-1;
 
-        while(p1 >=0 || p2 >= 0){
-            if(p1<0){
+        while(p1>=0 && p2>=0){
+            if(nums1[p1] > nums2[p2]){
+                nums1[i--] = nums1[p1--];
+            } else {
                 nums1[i--] = nums2[p2--];
             }
-            else if(p2<0){
-                nums1[i--] = nums1[p1--];
-            }
-
-            else {
-                nums1[i--] = max(nums1[p1], nums2[p2]);
-                if(nums1[p1] > nums2[p2]) p1--;
-                else p2--;
-            }
         }
+
+        while(p2 >= 0){
+            nums1[i--] = nums2[p2--];
+        }
+
     }
 };
